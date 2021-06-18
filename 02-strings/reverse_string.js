e77958df255382265fb03d36a5d2118e13674c2c
eyosiyas <eyosiyas@AULA1D-DOCENTE.edu-il.it>
2021-06-18 14:20:13 +0200

    ex_02_reverse done

--- 02-strings/reverse_string.js
+++ 02-strings/reverse_string.js
@@ -1,1 +1,10 @@
 // read carefully the instructions
+myString = "hello";
+let stringArray = myString
+
+stringArray = myString.split('')
+console.log(stringArray);
+stringArray = stringArray.reverse();
+console.log(stringArray);
+stringArray = stringArray.join('');
+console.log(stringArray);
