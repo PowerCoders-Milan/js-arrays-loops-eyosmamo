1380d632c35ce954f60fae7ea5341d058407ff3d
eyosiyas <eyosiyas@AULA1D-DOCENTE.edu-il.it>
2021-06-21 12:41:32 +0200

    ex.fibonacci done

--- 05-fibonacci/fibonacci.js
+++ 05-fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
 // program to generate fibonacci series up to a certain number
-const maxNumber = Math.ceil(Math.random()*200);
-let n1 = 0, n2 = 1, nextTerm;
+const maxNumber = Math.ceil(Math.random() * 10);
+function fibonacci(num) {
+    if (num == 1)
+        return 0;
+    if (num == 2)
+        return 1;
+    var num1 = 0;
+    var num2 = 1;
+    var sum;
+    var i = 2;
+    while (i < num) {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+        i += 1;
+    }
+}
 
+
+
+console.log(maxNumber);
