58c7fb3e64480827d4e76a6279c9453b99e0b416
eyosiyas <eyosiyas@AULA1D-DOCENTE.edu-il.it>
2021-06-21 10:50:21 +0200

    ex.03_multiples_of_number done

--- 03-multiples-of-number/multiples_of_number.js
+++ 03-multiples-of-number/multiples_of_number.js
@@ -1,1 +1,12 @@
 // define your function here
+
+const multiplesOf = (num, lim) => {
+    const multiples = [];
+    for (let i = 1; i <= lim; i++) {
+        multiples.push(num * i);
+    }
+    return multiples;
+
+}
+
+console.log(multiplesOf(5, 10));
