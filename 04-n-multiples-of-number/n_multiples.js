f9b4194efc6cf72e1d772864e4e2c4ff51c47a91
eyosiyas <eyosiyas@AULA1D-DOCENTE.edu-il.it>
2021-06-21 11:39:52 +0200

    ex.n_multiples_of_number done

--- 04-n-multiples-of-number/n_multiples.js
+++ 04-n-multiples-of-number/n_multiples.js
@@ -1,6 +1,13 @@
 // function declaration
-const nMultiplesOf = (number, limit) => {
+const nMultiplesOf = (num, lim) => {
+    const nmultiplesofArray = []
+    let i = 0;
+    while (nmultiplesofArray.length <= lim) {
 
+        nmultiplesofArray.push(num * i);
+        i++;
+    }
+    return nmultiplesofArray;
 }
 
-console.log(nMultiplesOf(2, 10))
+console.log(nMultiplesOf(2, 20))
